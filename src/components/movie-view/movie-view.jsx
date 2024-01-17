9f41c4e0424d9925affb469f4e2983baa6cb2f70
Jorgec88 <j.carralero88@gmail.com>
2024-01-16 19:06:56 -0500

    Update
    Adding a navbar

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,10 +1,15 @@
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import './movie-view.scss';
 
-export const MovieView = ({ movie, onBackClick }) => {
+export const MovieView = ({ movie }) => {
+  const { movieId } = useParams();
+  const movie = movies.find((b) => b.id === movieId);
+
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img className='w-100' src={movie.image} />
       </div>
       <div>
         <span>Title: </span>
@@ -14,13 +19,9 @@ export const MovieView = ({ movie, onBackClick }) => {
         <span>Director: </span>
         <span>{movie.director}</span>
       </div>
-      <button
-        onClick={onBackClick}
-        className='back-button'
-        style={{ cursor: 'pointer' }}
-      >
-        Back
-      </button>
+      <Link to={`/`}>
+        <button className='back-button'>Back</button>
+      </Link>
     </div>
   );
 };
